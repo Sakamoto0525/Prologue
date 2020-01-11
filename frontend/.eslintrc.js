e260f64312acf2425ef93e042b6a728fa30e0126
Sakamoto0525 <525masaya@gmail.com>
2020-01-11 14:26:54 +0900

    .eslintrc.js追加

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,19 +2,28 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    "jest/globals": true
   },
   parserOptions: {
     parser: 'babel-eslint'
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'plugin:prettier/recommended'
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:vue/recommended',
+    'prettier/vue'
   ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
-  rules: {}
+  plugins: ['vue', 'prettier', "jest"],
+  rules: {
+    semi: [2, 'never'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-v-html': 'off',
+    'prettier/prettier': ['error', {
+      semi: false,
+      singleQuote: true
+    }]
+  }
 }
